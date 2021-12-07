879ad59f7bfd21998d3c363b5f5d12decc8bd299
rhertle <hertle@puzzle.ch>
2021-12-07 14:54:08 +0100

    snapshot version

--- init-registry.sh
+++ init-registry.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # publish the platform to the registry
-jbang catalog_publish@quarkusio --working-directory=. --registry-url=http://localhost:8085 --token=test --all
+jbang catalog_publish.java --working-directory=. --registry-url=http://localhost:8085 --token=d53298e5075c7ece3f39925aeb52ab4e --all
